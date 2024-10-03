bc0f10bc0072d316388e7a56d783200dc77bedc5
Wesley Alves <pro.wesleyalves@gmail.com>
2024-10-03 15:43:53 -0300

    Portifolio

---
+++ Portifolio/js/main.js
@@ -0,0 +1,11 @@
+function darkLigth() {
+  const html = document.documentElement;
+
+  html.classList.toggle("ligth");
+
+  // if (html.classList.contains('ligth')) {
+  //   html.classList.remove('ligth');
+  // } else {
+  //    html.classList.add('ligth');
+  //  }
+}
